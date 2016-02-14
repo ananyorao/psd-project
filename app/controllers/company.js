474b7741db9667d39eeef0c9a0d9565bfae6c132
Adheesh Bhatia <adheeshbhatia@gmail.com>
2016-02-14 14:55:42 +0530

    fixed the synopsis data with newsletter

--- app/controllers/company.js
+++ app/controllers/company.js
@@ -29,20 +29,46 @@ module.exports = function (db, utils) {
       async.waterfall([
           function(callback) {
             var cid = req.params.cid;
-              db.Company.find({'_id': cid}, function(err,data) {
+              db.Company.find({'_id': cid}).lean().exec(function(err,data) {
                 if(err) {
                   console.log(err);
                   res.status(404).json({ error:'Invalid GET request' });
                   return;
                 }
                 callback(null, data);
-              })
+              });
           },
           function(data,callback) {
             var companyName = data[0].name;
-            db.Synopsis.find({'company' : companyName}, function(err,data) {
+            db.Synopsis.find({'company' : companyName}).lean().exec(function(err,data) {
               callback(null, data);
-            })
+            });
+          },
+          function(data,callback) {
+            var newsletterIndex = [];
+            var _ = require('underscore');
+            var ids = _(data).chain().flatten().pluck('idno').unique().value();
+            
+            db.Newsletter.find({id: {$in: ids}}).lean().exec(function(err,newsletters) {
+              if(err) {
+                console.log(err);
+                return;
+              }
+              var synopsisWithNewsletter = _(data).chain().flatten().map(function(synopsis) {
+              var id = synopsis.idno;
+              for(i=0; i< newsletters.length; i++) {
+                var idFromNewsletter = newsletters[i].id;
+                if(idFromNewsletter.indexOf(synopsis.idno) != -1) {
+                  synopsis.newsletter = newsletters[i].feedback;
+                } else {
+                  synopsis.newsletter = "";
+                }
+              }
+                return synopsis;
+              }).value();
+              console.log(synopsisWithNewsletter);
+              callback(null,synopsisWithNewsletter);
+            });
           }
         ], function(err, result) {
           if(err) {
